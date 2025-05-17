0260533850fbe78d1ef16c03b989200a0dbbb458
rodiond26 <rodiond26@gmail.com>
2025-05-17 21:57:42 +0500

    webinar 3

--- src/main/scala/ru/otus/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/ru/otus/module1/03-introducing_fp_concepts.scala
@@ -1,20 +1,15 @@
 package ru.otus.module1
 
-import ru.otus.module1.opt.{Animal, Cat, Dog}
+import ru.otus.module1.opt.{Animal, Cat}
 
 import scala.annotation.tailrec
 import scala.language.postfixOps
-
-
 
 /**
  * referential transparency
  */
 
-
-
-
- // recursion
+// recursion
 
 object recursion {
 
@@ -26,29 +21,23 @@ object recursion {
   def fact(n: Int): Int = {
     var _n = 1
     var i = 2
-    while (i <= n){
+    while (i <= n) {
       _n *= i
       i += 1
     }
     _n
   }
 
-
-  def factRec(n: Int): Int = if(n <= 0) 1 else n * factRec(n - 1)
-
+  def factRec(n: Int): Int = if (n <= 0) 1 else n * factRec(n - 1)
 
   def factTailRec(n: Int, accum: Int): Int = {
     @tailrec
     def loop(n: Int, accum: Int): Int =
-      if(n <= 0) accum
+      if (n <= 0) accum
       else loop(n - 1, n * accum)
 
     loop(n, 1)
   }
-
-
-
-
 
   /**
    * реализовать вычисление N числа Фибоначчи
@@ -58,9 +47,7 @@ object recursion {
 
 }
 
-
-
-object hof{
+object hof {
 
   def dumb(string: String): Unit = {
     Thread.sleep(1000)
@@ -68,70 +55,40 @@ object hof{
   }
 
   // обертки
-
   def logRunningTime[A, B](f: A => B): A => B = a => {
-      val start = System.currentTimeMillis()
-      val result = f(a)
-      val end = System.currentTimeMillis()
-      println(s"Running time: ${end - start}")
-      result
+    val start = System.currentTimeMillis()
+    val result = f(a)
+    val end = System.currentTimeMillis()
+    println(s"Running time: ${end - start}")
+    result
   }
 
-
-
   // изменение поведения ф-ции
-
   def isOdd(i: Int): Boolean = i % 2 > 0
+
   def not[A](f: A => Boolean): A => Boolean = a => !f(a)
+
   val isEven: Int => Boolean = not(isOdd)
 
   isOdd(5) // true
   isEven(5) // false
 
-
   // изменение самой функции
-
   def sum(x: Int, y: Int): Int = x + y
 
   def partial[A, B, C](a: A)(f: (A, B) => C): B => C =
     f.curried(a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
-
-
-
-
-
 /**
- *  Реализуем тип Option
+ * Реализуем тип Option
  */
-
-
-
- object opt {
-
-
+object opt {
 
   trait Animal
+
   case class Cat() extends Animal
+
   case class Dog() extends Animal
 
   /**
@@ -143,7 +100,7 @@ object hof{
   // + Covariance Если А является подтипом В, то Option[A] является подтипом Option[B]
   // - Contravariance Если А является подтипом В, то Option[A] является супер типом Option[B]
 
-    // Function1[-R, +T]
+  // Function1[-R, +T]
   val f1: String => Unit = ???
   val f2: Any => Unit = ???
 
@@ -152,9 +109,8 @@ object hof{
   foo(f2)
   foo(f1)
 
-
   sealed trait Option[+T] {
-    def isEmpty: Boolean = if(this.isInstanceOf[None.type]) true else false
+    def isEmpty: Boolean = if (this.isInstanceOf[None.type]) true else false
 
 //    def get: T =  if(this.isInstanceOf[None.type]) throw new Exception("None get")
 //      else{
@@ -173,16 +129,12 @@ object hof{
 
   object Option {
     def apply[T](v: T): Option[T] =
-      if(v == null) None else Some(v)
+      if (v == null) None else Some(v)
   }
 
-  val opt1 : Option[Int] = ???
-
+  val opt1: Option[Int] = ???
   val opt2: Option[Option[Int]] = opt1.map(i => Option(i + 1))
   val opt3: Option[Int] = opt1.flatMap(i => Option(i + 1))
-
-
-
 
   /**
    *
@@ -201,78 +153,74 @@ object hof{
    * Реализовать метод filter, который будет возвращать не пустой Option
    * в случае если исходный не пуст и предикат от значения = true
    */
+}
 
- }
+object list {
+  /**
+   * Реализовать одно связанный иммутабельный список List
+   * Список имеет два случая:
+   * Nil - пустой список
+   * Cons - непустой, содержит первый элемент (голову) и хвост (оставшийся список)
+   */
 
- object list {
-   /**
-    *
-    * Реализовать одно связанный иммутабельный список List
-    * Список имеет два случая:
-    * Nil - пустой список
-    * Cons - непустой, содержит первый элемент (голову) и хвост (оставшийся список)
-    */
+  def treat(a: Option[Animal]) = ???
 
-   def treat(a: Option[Animal]) = ???
+  sealed trait List[+T] {
 
-    sealed trait List[+T] {
+    // prepend
+    def ::[TT >: T](elem: TT): List[TT] = ???
+  }
 
-     // prepend
-     def ::[TT >: T](elem: TT): List[TT] = ???
-   }
-    case class ::[T](elem: T, tail: List[T]) extends List[T]
-    case object Nil extends List[Nothing]
+  case class ::[T](elem: T, tail: List[T]) extends List[T]
 
-   object List {
-     def apply[A](v: A*): List[A] =
-       if(v.isEmpty) Nil
-       else new ::(v.head, apply(v.tail :_*))
-   }
+  case object Nil extends List[Nothing]
 
-   val l1 = List(1, 2, 3)
+  object List {
+    def apply[A](v: A*): List[A] =
+      if (v.isEmpty) Nil
+      else new::(v.head, apply(v.tail: _*))
+  }
 
-   val l2: List[Cat] = List(Cat())
+  val l1 = List(1, 2, 3)
 
+  val l2: List[Cat] = List(Cat())
 
+  /**
+   * Конструктор, позволяющий создать список из N - го числа аргументов
+   * Для этого можно воспользоваться *
+   *
+   * Например, вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
+   * def printArgs(args: Int*) = args.foreach(println(_))
+   */
 
+  /**
+   *
+   * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
+   */
 
+  /**
+   *
+   * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
+   */
 
+  /**
+   *
+   * Реализовать метод filter для списка который будет фильтровать список по некому условию
+   */
 
-    /**
-      * Конструктор, позволяющий создать список из N - го числа аргументов
-      * Для этого можно воспользоваться *
-      * 
-      * Например, вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
-      * def printArgs(args: Int*) = args.foreach(println(_))
-      */
+  /**
+   *
+   * Написать функцию incList которая будет принимать список Int и возвращать список,
+   * где каждый элемент будет увеличен на 1
+   */
+  def incList(arr: Array[Int]): Array[Int] = arr.map(_ + 1)
 
-    /**
-      *
-      * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
-      */
-
-    /**
-      *
-      * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
-      */
-
-
-    /**
-      *
-      * Реализовать метод filter для списка который будет фильтровать список по некому условию
-      */
-
-    /**
-      *
-      * Написать функцию incList которая будет принимать список Int и возвращать список,
-      * где каждый элемент будет увеличен на 1
-      */
-
-
-    /**
-      *
-      * Написать функцию shoutString которая будет принимать список String и возвращать список,
-      * где к каждому элементу будет добавлен префикс в виде '!'
-      */
-
- }
+  /**
+   *
+   * Написать функцию shoutString которая будет принимать список String и возвращать список,
+   * где к каждому элементу будет добавлен префикс в виде '!'
+   */
+  def shoutString(arr: Array[String]): Array[String] = arr.map('!' + _)
+  
+  val a = shoutString(Array("a", "b", "c"))
+}
