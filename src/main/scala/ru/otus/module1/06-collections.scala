4fde4eec6399fb7effaaab5b49d2d32ac502572a
rodiond26 <rodiond26@gmail.com>
2025-05-26 10:38:14 +0500

    webinar 5-6

--- src/main/scala/ru/otus/module1/06-collections.scala
+++ src/main/scala/ru/otus/module1/06-collections.scala
@@ -2,12 +2,11 @@ package ru.otus.module1
 
 import scala.annotation.tailrec
 
-
 object collections2 {
-
 
   sealed trait List[+T] {
     def head: T
+
     def tail: List[T]
 
     def isEmpty: Boolean = this match {
@@ -28,13 +27,12 @@ object collections2 {
     }
 
     def flatMap2[B](f: T => List[B]): List[B] =
-      foldLeft(List.empty[B]){
+      foldLeft(List.empty[B]) {
         case (acc, el) =>
           acc ::: f(el)
       }
 
     def map[B](f: T => B): List[B] = flatMap(t => Cons(f(t), Nil))
-
 
     @tailrec
     final def foldLeft[B](acc: B)(f: (B, T) => B): B = this match {
@@ -43,23 +41,22 @@ object collections2 {
     }
 
     def take(n: Int): List[T] = {
-      val r = foldLeft((0, List.empty[T])){
+      val r = foldLeft((0, List.empty[T])) {
         case ((i, acc), el) =>
-          if(i == n) (i, acc)
+          if (i == n) (i, acc)
           else (i + 1, el :: acc)
       }
       r._2
     }
 
     def drop(n: Int): List[T] = ???
-
-
   }
 
   case class Cons[T](head: T, tail: List[T]) extends List[T]
 
-  case object Nil extends List[Nothing]{
+  case object Nil extends List[Nothing] {
     def head = throw new NoSuchElementException("Nil.head")
+
     def tail = throw new UnsupportedOperationException("Nil.tail")
   }
 
@@ -70,11 +67,10 @@ object collections2 {
   val l1 = Cons(1, Cons(2, Cons(3, Nil)))
   val l2 = Cons(4, Cons(5, Cons(6, Nil)))
 
-  val l3: List[(Int, Int)] = for{
+  val l3: List[(Int, Int)] = for {
     e1 <- l1
     e2 <- l2
   } yield (e1, e2)
 
   val l4 = l1.flatMap(e1 => l2.map(e2 => (e1, e2)))
-
 }
