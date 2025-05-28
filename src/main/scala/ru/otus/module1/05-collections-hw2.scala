eb39535e7d473bbb8443a66d75331992a2950a71
rodiond26 <rodiond26@gmail.com>
2025-05-28 12:06:27 +0500

    hw2 complete

--- src/main/scala/ru/otus/module1/05-collections-hw2.scala
+++ src/main/scala/ru/otus/module1/05-collections-hw2.scala
@@ -1,48 +1,48 @@
 package ru.otus.module1
 
-import scala.annotation.tailrec
 import scala.util.Random
 
 object hw2 {
 
   sealed trait Ball
 
-  object BlackBall extends Ball
+  case object BlackBall extends Ball
 
-  object WhiteBall extends Ball
+  case object WhiteBall extends Ball
 
-  val balls: List[Ball] = List(BlackBall, BlackBall, BlackBall, WhiteBall, WhiteBall, WhiteBall)
+  private val urn: List[Ball] = List(BlackBall, BlackBall, BlackBall, WhiteBall, WhiteBall, WhiteBall)
 
-  def getFirstIndex(size: Int): Int = Random.between(0, size)
+  def shuffleUrn(): List[Ball] = Random.shuffle(urn)
 
-  @tailrec
-  def getSecondIndex(size: Int, firstIndex: Int): Int = {
-    val index = Random.between(0, size)
-
-    if (index == firstIndex) getSecondIndex(size, firstIndex)
-    else index
+  def hasWhiteBallAtFirstOrSecond(urn: List[Ball]): Boolean = {
+    urn.head == WhiteBall || urn(1) == WhiteBall
   }
 
-  def oneExperiment(balls: List[Ball]): Boolean = {
-    val first = getFirstIndex(balls.size)
-    val second = getSecondIndex(balls.size, first)
-
-    (balls(first), balls(second)) match {
-      case (WhiteBall, _) => true
-      case (BlackBall, WhiteBall) => true
-      case _ => false
-    }
+  def makeExperiment(count: Int): Int = {
+    val experiments: Seq[List[Ball]] = (1 to count).map(_ => shuffleUrn()).filter(hasWhiteBallAtFirstOrSecond)
+    experiments.take(count).size
   }
 
   def main(args: Array[String]): Unit = {
-    val experiments: Seq[BallsExperiment] = (1 to count).map(_ => new BallsExperiment())
 
+    val count1000 = 1_000
+    val count10000 = 10_000
+    val count100000 = 100_000
+    val count1000000 = 1_000_000
 
+    val experiment1000: Int = makeExperiment(count1000)
+    val experiment10000: Int = makeExperiment(count10000)
+    val experiment100000: Int = makeExperiment(count100000)
+    val experiment1000000: Int = makeExperiment(count1000000)
 
+    println(s"Result: $experiment1000 from $count1000")
+    println(s"Result: $experiment10000 from $count10000")
+    println(s"Result: $experiment100000 from $count100000")
+    println(s"Result: $experiment1000000 from $count1000000")
 
-    println(oneExperiment(balls))
-    println(oneExperiment(balls))
-    println(oneExperiment(balls))
-    println(oneExperiment(balls))
+    //    Result: 796 from 1000
+    //    Result: 7925 from 10000
+    //    Result: 80034 from 100000
+    //    Result: 800676 from 1000000
   }
 }
